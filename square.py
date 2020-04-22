556f0d9bf7bcba7792aaada6997e73b66816efc6
bharathkumarreddy19 <64121431+bharathkumarreddy19@users.noreply.github.com>
2020-04-22 19:47:29 +0530

    Add files via upload
    Area and Perimeter of Square

---
+++ square.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 22 19:04:25 2020
+
+@author: bhara_5sejtsc
+"""
+
+def areaSquare(l): 
+	return (l * l) 
+
+def perimeterSquare(l): 
+	return (4 * l) 
+
+
+l = int(input("length = ")); 
+
+print ("Area = ", areaSquare(l)) 
+print ("Perimeter = ", perimeterSquare(l)) 
+
+
